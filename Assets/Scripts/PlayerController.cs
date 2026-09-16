9dc2a8fd7fa5ed67ab6161a89ae392d4b782f40f
Don Brogdon <dbrogdon91@gmail.com>
2026-09-15 20:40:41 -0500

    Move player with Rigidbody2D physics

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,11 +3,15 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float moveSpeed = 5f;
+    private Rigidbody2D rb;
+    private Vector2 movement;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         Debug.Log("Game Started");
+        //Looks at the GameObject this script is attached to and give it the Rigidbody2D component
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -18,11 +22,21 @@ public class PlayerController : MonoBehaviour
         //Tells me the players vertical input right now
         float vertical = Input.GetAxisRaw("Vertical");
         // Combines horizontal and vertical input into one movement vector giving us the direction and magnitude
-        Vector2 movement = new Vector2(horizontal, vertical).normalized;
+        //Vector2 movement = new Vector2(horizontal, vertical).normalized;
+        movement = new Vector2(horizontal, vertical).normalized;
 
-        //Handles the movement of the player
-        transform.Translate(movement.x * moveSpeed * Time.deltaTime, movement.y * moveSpeed * Time.deltaTime, 0);
+        //Handles the movement of the player without RigidBody2D
+        //transform.Translate(movement.x * moveSpeed * Time.deltaTime, movement.y * moveSpeed * Time.deltaTime, 0);
 
+        //Handles the movement of the player with RigidBody2D
+        //distance = speed * time
+        //rb.MovePosition(rb.position + movement * moveSpeed * Time.deltaTime);
        
+    }
+
+    //Physics system to operate on a fixed time interval
+    void FixedUpdate() {
+        //Handles player movement with Unity's physic engine
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
     }
 }
